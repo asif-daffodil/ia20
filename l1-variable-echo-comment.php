b68012b1ccf40d3833b1ef5ce4c8d1e4ab7dc8b4
asif-daffodil <abir@dipti.com.bd>
2023-08-26 16:41:14 +0600

    Aug-26-2023

---
+++ l1-variable-echo-comment.php
@@ -0,0 +1,20 @@
+<?php
+$x = "Hello World";
+echo $x;
+
+// 1 line comment
+
+/* this
+is 
+a
+multiline comment
+*/
+
+# this is also a single line comment
+
+/**
+ * this is
+ * also a
+ * multiline
+ * comment
+ */
